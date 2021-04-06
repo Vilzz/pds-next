def935ef4554fd96bc919196beaa903a1ed6c5f3
Dmitry Baklanov <n79613928275@gmail.com>
2021-04-06 17:30:10 +0400

    Product description page

--- pages/catalog/[slug]/[subslug]/[id].js
+++ pages/catalog/[slug]/[subslug]/[id].js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import { useRouter } from 'next/router'
-import { Row, Col, Container } from 'react-bootstrap'
+import { Row, Col, Container, Button } from 'react-bootstrap'
 import Image from 'next/image'
 import Head from 'next/head'
-const Product = ({ group }) => {
+import Link from 'next/link'
+import Catalogheader from '../../../../components/bredcrambs/Catalogheader.jsx'
+const Product = ({ group, subcategory }) => {
   const router = useRouter()
-  const { id } = router.query
+  const { id, slug, subslug } = router.query
   return (
     <div>
       <Head>
@@ -13,9 +15,15 @@ const Product = ({ group }) => {
         <meta name='description' content={group.data.name} lang='ru-RU' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
-      <Container className='tmp'>
+      <Catalogheader
+        slug={slug}
+        subslug={subslug}
+        name={subcategory.data[0].name}
+        subname={subcategory.data[0].name}
+      />
+      <Container className='my-4'>
         <Row className='devbd'>
-          <h2>{group.data.name}</h2>
+          <h2>Тут будет что-то</h2>
         </Row>
         <Row>
           <Col sm={12} md={6}>
@@ -48,25 +56,113 @@ const Product = ({ group }) => {
                   width={400}
                   height={400}
                   src={group.data.big_img}
-                  alt='Текст'
+                  alt={group.data.name}
                 />
               </Col>
             </Row>
+            <Row>
+              <h6>Описание:</h6>
+              <p className='font-size-sm'>{group.data.description}</p>
+            </Row>
           </Col>
-          <Col sm={12} md={6}></Col>
+          <Col sm={12} md={6}>
+            <div className='h-100 bg-light rounded-lg py-5 px-4 px-sm-5 devbd'>
+              <Link href='#'>
+                <a className='product-meta d-block font-size-sm pb-2'>
+                  {subcategory.data[0].name}
+                </a>
+              </Link>
+
+              <h1 className='h2'>{group.data.name}</h1>
+              <Row className='align-items-center'>
+                <Col>Артикул: {group.data.article}</Col>
+                <Col>
+                  <div className='h2 font-weight-normal text-accent text-right'>
+                    <small>Цена: </small>
+                    {group.data.price}
+                    <small>руб.</small>
+                  </div>
+                </Col>
+              </Row>
+
+              <div className='form-group d-flex flex-wrap align-items-center pt-4 pb-2'>
+                <select
+                  className='custom-select mr-3 mb-3'
+                  style={{ width: '5rem' }}
+                >
+                  <option value='1'>1</option>
+                  <option value='2'>2</option>
+                  <option value='3'>3</option>
+                  <option value='4'>4</option>
+                  <option value='5'>5</option>
+                </select>
+                <Button
+                  variant='primary'
+                  className='btn-shadow mr-3 mb-3'
+                  type='submit'
+                >
+                  <i className='czi-cart font-size-lg mr-2'></i>Заказать
+                </Button>
+                <button
+                  className='btn btn-icon btn-secondary mb-3'
+                  type='submit'
+                  data-toggle='tooltip'
+                  title='Add to Wishlist'
+                >
+                  <i className='czi-heart font-size-lg'></i>
+                </button>
+              </div>
+
+              <Row>
+                <Col>
+                  <h6>Материалы:</h6>
+                  <ul className='list-unstyled font-size-sm pt-2 mb-0'>
+                    {group.data.material.split(';').map((mt) => (
+                      <li key={mt}>
+                        <i className='czi-check-circle text-success mr-2'></i>
+                        {mt}
+                      </li>
+                    ))}
+                  </ul>
+                </Col>
+                <Col>
+                  <h6>Размеры:</h6>
+                  <ul className='list-unstyled font-size-sm pt-2 mb-0'>
+                    {group.data.product_size.split(';').map((sz) => (
+                      <li key={sz}>
+                        <i className='czi-check-circle text-info mr-2'></i>
+                        {sz}
+                      </li>
+                    ))}
+                  </ul>
+                </Col>
+              </Row>
+              <Row className='d-flex justify-content-center align-items-end'>
+                <Link href={`/catalog/${slug}/${subslug}`}>
+                  <a className='btn btn-info'>
+                    <i className='czi-reply font-size-lg mr-2'></i>В каталог
+                  </a>
+                </Link>
+              </Row>
+            </div>
+          </Col>
         </Row>
       </Container>
     </div>
   )
 }
 export const getServerSideProps = async (context) => {
-  const { id } = context.params
+  const { id, subslug } = context.params
   const product = await axios.get(
     `${process.env.NEXT_PUBLIC_DEV_SERVER}/groups/${id}`
+  )
+  const subctg = await axios.get(
+    `${process.env.NEXT_PUBLIC_DEV_SERVER}/subcategories?slug=${subslug}&select=name,parent,-groups&limit=1`
   )
   return {
     props: {
       group: product.data,
+      subcategory: subctg.data,
     },
   }
 }
