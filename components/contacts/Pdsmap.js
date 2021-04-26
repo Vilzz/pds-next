2f2d1a2f08ffe64040604c2a257b53cb3fa191a5
Dmitry Baklanov <n79613928275@gmail.com>
2021-04-26 15:29:26 +0400

    Contact page finished

--- components/contacts/Pdsmap.js
+++ components/contacts/Pdsmap.js
@@ -6,11 +6,12 @@ const icon = Leaflet.icon({
   iconRetinaUrl: '/images/icons/marker-icon-2x.png',
   shadowUrl: '/images/icons/marker-shadow.png',
 })
+
 const Pdsmap = () => {
   return (
     <MapContainer
       center={points.office.coords}
-      zoom={15}
+      zoom={points.zoom}
       scrollWheelZoom={true}
     >
       <TileLayer
@@ -18,7 +19,16 @@ const Pdsmap = () => {
         url={points.basemap.osm}
       />
       <Marker position={points.office.coords} icon={icon}>
-        <Popup>{points.office.name}</Popup>
+        <Popup position={points.office.coords}>
+          <div className='d-flex flex-column align-items-center'>
+            <img
+              width='200'
+              src='/images/logo.jpg'
+              alt='Оффис ООО "Про-движение"'
+            />
+            <p className='text-muted'>{points.office.address}</p>
+          </div>
+        </Popup>
       </Marker>
     </MapContainer>
   )
@@ -28,7 +38,7 @@ export default Pdsmap
 
 const points = {
   office: {
-    name: 'Про-движение',
+    name: 'ООО "Про-движение"',
     coords: {
       lat: 53.192835,
       lng: 50.268502,
@@ -36,16 +46,7 @@ const points = {
     phones: ['+72223567687', '+74443567687'],
     address: 'Самарская обл. г. Самара Полевой проезд дом 12',
   },
-  warhouse: {
-    name: 'Производство',
-    coords: {
-      lat: 53.192835,
-      lng: 50.268502,
-    },
-    phones: ['+76663567687', '+75553567687'],
-    address: 'Самарская обл. г. Самара Полевой проезд дом 12',
-  },
-  zoom: 15,
+  zoom: 17,
   basemap: {
     osm: 'https://{s}.tile.osm.org/{z}/{x}/{y}.png',
     hot: 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
--- pages/contacts.js
+++ pages/contacts.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 
 const PDSMap = dynamic(() => import('../components/contacts/Pdsmap'), {
   ssr: false,
@@ -8,12 +8,67 @@ const PDSMap = dynamic(() => import('../components/contacts/Pdsmap'), {
 const Contacts = () => {
   return (
     <Container className='mb-5'>
-      {/* <Head>
+      <Head>
         <title>Про-движение | Контакты</title>
         <link rel='icon' href='/favicon.ico' />
-      </Head> */}
-      <h1 className='display-4 text-center py-4'>Контактная информация</h1>
-      <PDSMap />
+        <meta
+          name='description'
+          content='Контактная информация ООО Про-движение'
+        />
+      </Head>
+
+      <Row className='d-flex align-items-center'>
+        <Col md={4}>
+          <h2 className='text-center py-md-3'>Контактная информация</h2>
+          <div className='card border-0 box-shadow'>
+            <div className='card-body'>
+              <ul className='list-unstyled mb-0'>
+                <li className='media pb-3 border-bottom'>
+                  <i className='czi-location font-size-lg mt-2 mb-0 text-primary'></i>
+                  <div className='media-body pl-3'>
+                    <span className='font-size-ms text-muted'>Наш адрес</span>
+                    <p className='lead text-muted'>
+                      Самарская обл. г. Самара Полевой проезд дом 12
+                    </p>
+                  </div>
+                </li>
+                <li className='media pt-2 pb-3 border-bottom'>
+                  <i className='czi-phone font-size-lg mt-2 mb-0 text-primary'></i>
+                  <div className='media-body pl-3'>
+                    <span className='font-size-ms text-muted'>Наш телефон</span>
+                    <p className='lead text-muted'>
+                      <a href='tel:+184725276533'>+7 222 3567687</a>
+                    </p>
+                  </div>
+                </li>
+                <li className='media pt-2 pb-3 border-bottom'>
+                  <i className='czi-mail font-size-lg mt-2 mb-0 text-primary'></i>
+                  <div className='media-body pl-3'>
+                    <span className='font-size-ms text-muted'>Наша почта</span>
+                    <p className='text-muted lead'>
+                      <a href='mailto:email@example.com'>email@example.com</a>
+                    </p>
+                  </div>
+                </li>
+                <li className='media pt-2 pb-3'>
+                  <i className='czi-telegram font-size-lg mt-2 mb-0 text-primary'></i>
+                  <div className='media-body pl-3'>
+                    <span className='font-size-ms text-muted'>
+                      Наш телеграм
+                    </span>
+                    <p className='text-muted lead'>
+                      <a href='tg://msg?text=pro-dvijenie'>Про-движение</a>
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Col>
+        <Col md={8}>
+          <PDSMap />
+        </Col>
+      </Row>
     </Container>
   )
 }
